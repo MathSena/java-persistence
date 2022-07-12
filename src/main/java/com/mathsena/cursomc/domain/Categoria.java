27882c0eb77adcfba8ae6c8b0369eefdd4d65179
Matheus Sena <MathSena07@hotmail.com>
2022-07-12 12:40:34 -0300

    Testando a primeira classe de domínio - Categoria

---
+++ src/main/java/com/mathsena/cursomc/domain/Categoria.java
@@ -0,0 +1,64 @@
+package com.mathsena.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+	// 1 - atributos
+	private Integer id;
+	private String nome;
+	
+	// 2 - contrutores
+	public Categoria() {
+		
+	}
+	
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	// 3 - Getters and Setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	// 4 - HashCodes e equals()
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	// 5- Serializable
+
+}
