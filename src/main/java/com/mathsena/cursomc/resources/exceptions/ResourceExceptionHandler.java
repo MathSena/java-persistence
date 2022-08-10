544c7c779bb06cd20283371eb1f984d3fcdf1fac
Matheus Sena <MathSena07@hotmail.com>
2022-08-10 17:56:29 -0300

    Deletando uma Categoria com Delete

--- src/main/java/com/mathsena/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/mathsena/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.mathsena.cursomc.resources.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.mathsena.cursomc.services.expections.DataIntegrityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,14 +13,18 @@ import com.mathsena.cursomc.services.expections.ObjectNotFoundException;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
-		
-		
+	}
+
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 
 }
--- src/main/java/com/mathsena/cursomc/services/CategoriaService.java
+++ src/main/java/com/mathsena/cursomc/services/CategoriaService.java
@@ -2,7 +2,9 @@ package com.mathsena.cursomc.services;
 
 import java.util.Optional;
 
+import com.mathsena.cursomc.services.expections.DataIntegrityException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.mathsena.cursomc.domain.Categoria;
@@ -34,4 +36,19 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+
+	public void delete(Integer id){
+		find(id);
+
+		try {
+			repo.deleteById(id);
+
+		}catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível deletar uma categoria que possui produtos");
+
+
+		}
+	}
+
+
 }
