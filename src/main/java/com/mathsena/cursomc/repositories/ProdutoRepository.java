2feb70fb646973ea64970e3e660a5be48bf85eb6
Matheus Sena <MathSena07@hotmail.com>
2022-08-13 13:30:41 -0300

    Busca de nome por produto e categorias

--- src/main/java/com/mathsena/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/mathsena/cursomc/repositories/ProdutoRepository.java
@@ -1,15 +1,24 @@
 package com.mathsena.cursomc.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.mathsena.cursomc.domain.Categoria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.mathsena.cursomc.domain.Produto;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 	Optional<Produto> findByid(Integer id);
-	
 
+    @Transactional(readOnly=true)
+    @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+    Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 }
---
+++ src/main/java/com/mathsena/cursomc/resources/utils/URL.java
@@ -0,0 +1,23 @@
+package com.mathsena.cursomc.resources.utils;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class URL {
+
+    public static String decodeParam(String s) {
+        return URLDecoder.decode(s, StandardCharsets.UTF_8);
+    }
+
+    public static List<Integer> decodeIntList(String s) {
+        String[] vet = s.split(",");
+        List<Integer> list = new ArrayList<>();
+        for (String value : vet) {
+            list.add(Integer.parseInt(value));
+        }
+        return list;
+        //return Arrays.asList(s.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+    }
+}
